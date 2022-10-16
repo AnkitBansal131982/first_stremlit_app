efe1b8832a619a675d18a9ace3d6b9b8f81df1b4
AnkitBansal131982 <115916480+AnkitBansal131982@users.noreply.github.com>
2022-10-16 21:09:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 
 streamlit.title('My New Healthy Breakfast')
 streamlit.header('🥑 Avacado')
-streamlit.text('🎂 Omega3 and Almonds')
+streamlit.text('🎂 Omega3 and Almonds Kavish')
 streamlit.text('😎,Guava and Banana')
 
