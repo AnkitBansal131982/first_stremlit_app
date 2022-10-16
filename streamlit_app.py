2d9785ba7e217ba87079fe6a40aa83c013ae887b
AnkitBansal131982 <115916480+AnkitBansal131982@users.noreply.github.com>
2022-10-16 22:04:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.header('🥑 Avacado')
 streamlit.text('🎂 Omega3 and Almonds Kavish')
 streamlit.text('😎,Guava and Banana')
 
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
